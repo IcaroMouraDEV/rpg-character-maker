ef0c0a69606d0700f36c42f9cd73f7cf9984c54f
Icaro Joel <icaromoura.programador@gmail.com>
2022-04-28 18:25:44 -0300

    modificando os buttons de mostrar os formularios, e add um button para ocultar os formularios!

--- src/script.js
+++ src/script.js
@@ -1,42 +1,49 @@
-const shownRace = document.getElementById('shown-race');
-const shownTitle = document.getElementById('shown-title');
-const shownPerson = document.getElementById('shown-person');
-const form = document.querySelectorAll('.shown');
+const shownRace = document.getElementById("shown-race");
+const shownTitle = document.getElementById("shown-title");
+const shownPerson = document.getElementById("shown-person");
+const occult = document.getElementById("occult");
+const form = document.querySelectorAll(".shown");
 let active;
 
-shownRace.addEventListener('click', () => {
-    active = document.querySelectorAll('.active');
+shownRace.addEventListener("click", () => {
+  active = document.querySelectorAll(".active");
 
-    if (active.length > 0) {
-        for (let i = 0; i < form.length; i += 1) {
-            form[i].className = '';
-        }
-    } else {
-        form[0].className = 'active';
+  if (active.length > 0) {
+    for (let i = 0; i < form.length; i += 1) {
+      form[i].className = "shown";
     }
+  }
+
+  form[0].className = "shown active";
 });
 
-shownTitle.addEventListener('click', () => {
-    active = document.querySelectorAll('.active');
+shownTitle.addEventListener("click", () => {
+  active = document.querySelectorAll(".active");
 
-    if (active.length > 0) {
-        for (let i = 0; i < form.length; i += 1) {
-            form[i].className = 'shown';
-        }
-    } else {
-        form[1].className = 'shown active';
+  if (active.length > 0) {
+    for (let i = 0; i < form.length; i += 1) {
+      form[i].className = "shown";
     }
+  }
+
+  form[1].className = "shown active";
 });
 
-shownPerson.addEventListener('click', () => {
-    active = document.querySelectorAll('.active');
+shownPerson.addEventListener("click", () => {
+  active = document.querySelectorAll(".active");
 
-    console.log(active.length)
-    if (active.length > 0) {
-        for (let i = 0; i < form.length; i += 1) {
-            form[i].className = '';
-        }
-    } else {
-        form[2].className = 'active';
+  console.log(active.length);
+  if (active.length > 0) {
+    for (let i = 0; i < form.length; i += 1) {
+      form[i].className = "shown";
     }
+  }
+
+  form[2].className = "shown active";
+});
+
+occult.addEventListener("click", () => {
+  for (let i = 0; i < form.length; i += 1) {
+    form[i].className = "shown";
+  }
 });
